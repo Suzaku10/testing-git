0ab79ae184cd5394f0e0b04bdd09ec0d002fe656
yudhistira.yoga <yudhistiray198@gmail.com>
2022-02-21 21:17:09 +0700

    Feature :  - Add File

---
+++ lib/yudis.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestingYudis extends StatefulWidget {
+  const TestingYudis({Key? key}) : super(key: key);
+
+  @override
+  _TestingYudisState createState() => _TestingYudisState();
+}
+
+class _TestingYudisState extends State<TestingYudis> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
